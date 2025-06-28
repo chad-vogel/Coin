1650eb9e9f14e0999a155f0a6a920ef26f53cdc1
Chad Vogel <chad@jumbobunny.com>
2025-06-28 16:59:06 -0400

    Use vendored protoc

--- proto/build.rs
+++ proto/build.rs
@@ -1,4 +1,8 @@
 fn main() {
     let proto_file = "proto/transaction.proto";
+    let protoc = protoc_bin_vendored::protoc_bin_path().expect("protoc not found");
+    unsafe {
+        std::env::set_var("PROTOC", protoc);
+    }
     prost_build::compile_protos(&[proto_file], &["proto"]).unwrap();
 }
