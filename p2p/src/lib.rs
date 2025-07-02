4353e5d71659175a21796f61b65c0ea7b908b4a3
Chad Vogel <chad@jumbobunny.com>
2025-07-01 21:33:32 -0400

    Add handshake timeouts

--- p2p/src/lib.rs
+++ p2p/src/lib.rs
@@ -819,7 +819,15 @@ impl Node {
         let mut stream = self.connect_stream(addr).await?;
         let hs = RpcMessage::Handshake(self.build_handshake());
         write_msg(&mut stream, &hs).await?;
-        let resp = read_msg(&mut stream).await?;
+        let resp = match tokio::time::timeout(Duration::from_secs(1), read_msg(&mut stream)).await {
+            Ok(r) => r?,
+            Err(_) => {
+                return Err(tokio::io::Error::new(
+                    tokio::io::ErrorKind::TimedOut,
+                    "handshake timeout",
+                ));
+            }
+        };
         match resp {
             RpcMessage::Handshake(h)
                 if h.network_id == self.network_id
@@ -851,7 +859,15 @@ impl Node {
         let mut stream = self.connect_stream(addr).await?;
         let hs = RpcMessage::Handshake(self.build_handshake());
         write_msg(&mut stream, &hs).await?;
-        let resp = read_msg(&mut stream).await?;
+        let resp = match tokio::time::timeout(Duration::from_secs(1), read_msg(&mut stream)).await {
+            Ok(r) => r?,
+            Err(_) => {
+                return Err(tokio::io::Error::new(
+                    tokio::io::ErrorKind::TimedOut,
+                    "handshake timeout",
+                ));
+            }
+        };
         match resp {
             RpcMessage::Handshake(h)
                 if h.network_id == self.network_id
